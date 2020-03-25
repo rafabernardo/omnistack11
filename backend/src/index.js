51b24a9c8d603553e62457d0b3091b5a20a858de
rafaela <rafabernardo.r@gmail.com>
2020-03-25 00:12:24 -0300

    Criação de routes com endpoint para criar a entidade ongs, listar. Criado endpoit para criar a entidade incidents, listar deletar. Criado endpoit para listar os incidents por id da ong

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
+
+/**
+ *  Metodos HTTP:
+ *  GET: Busca/listar info do back
+ *  POST: Cria info no back
+ *  PUT: Altera info no back
+ *  DELETE: Deletar info no back
+*/
+
+/**
+ *  Tipos de parametros:
+ *     - query params: parametros nomeados na rota apos "?" usado em filtros, paginação
+ *     - route params: parametros utilizados para indentificar recursos
+ *     - request body: corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+/**
+ * SQL: mysql, sqlite, postgreSQL
+ * NoSQL: mongoDB, couchDB 
+*/
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+*/
