bbc204298a127e9e2f2047654759961dcef853eb
rafaela <rafabernardo.r@gmail.com>
2020-03-25 09:51:03 -0300

    Finalizando dia 2 da semana

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -17,7 +17,24 @@ module.exports = {
   },
 
   async index(request, response) {
-    const incidents = await connection('incidents').select("*");
+    const { page = 1 } = request.query;
+
+    const [count] = await connection("incidents").count();
+
+    const incidents = await connection("incidents")
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
+      ]);
+
+    response.header("X-Total-Count", count["count(*)"]);
     return response.json(incidents);
   },
 
@@ -25,14 +42,19 @@ module.exports = {
     const { id } = request.params;
     const ong_id = request.headers.authorization;
 
-    const incident = await connection('incidents').where('id', id).select('ong_id').first();
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
 
     if (incident.ong_id != ong_id) {
-      return response.status(401).json({ error: 'Operation not permitted.'});
+      return response.status(401).json({ error: "Operation not permitted." });
     }
 
-    await connection('incidents').where('id', id).delete();
-   
+    await connection("incidents")
+      .where("id", id)
+      .delete();
+
     return response.status(204).send();
   }
 };
